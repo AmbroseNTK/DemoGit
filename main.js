4db69deaf92d231bf68cc3dea6297db7cb602947
Nguyễn Tuấn Kiệt <nguyentuankiet23031998@gmail.com>
2019-05-10 15:24:42 +0700

    Revert "Add dau file and modified"

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 sayHello();
-add(34,21);
 
 function sayHello() {
     alert("Hello World");
@@ -11,7 +10,4 @@ function add(a, b) {
 
 function subtract(a, b) {
     return a - b;
-}
-function product(a, b){
-    return a * b;
 }
